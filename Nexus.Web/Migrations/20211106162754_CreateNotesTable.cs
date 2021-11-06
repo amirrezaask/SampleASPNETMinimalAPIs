90c408e72110d7a1078522a015a43a89debe2b24
amirrezaask <raskarpour@gmail.com>
2021-11-06 20:08:59 +0330

    Notes CRUD APIs

---
+++ Nexus.Web/Migrations/20211106162754_CreateNotesTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexus.Web.Migrations
+{
+    public partial class CreateNotesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
--- Nexus.Web/Program.cs
+++ Nexus.Web/Program.cs
@@ -1,6 +1,69 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSqlite<NexusDbContext>("Data Source=nexus.db");
+}
+else
+{
+    builder.Services.AddNpgsql<NexusDbContext>(
+        builder.Configuration.GetConnectionString("Database"));
+}
+
+// Swagger
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
 
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nexus Cloud APIs");
+        c.RoutePrefix = String.Empty;
+    });
+
+}
+
+app.MapGet("/notes", async (NexusDbContext dbContext) => await dbContext.Notes.ToListAsync());
+app.MapPost("/notes", async (NexusDbContext dbContext, Note note) =>
+{
+    note.Id = Guid.NewGuid().ToString();
+    await dbContext.Notes.AddAsync(note);
+    await dbContext.SaveChangesAsync();
+    return note.Id;
+});
+app.MapGet("/notes/{id:guid}", async (string id, NexusDbContext dbContext) => await dbContext.Notes.FindAsync(id));
+app.MapDelete("/notes/{id:guid}", async (string id, NexusDbContext dbContext) =>
+{
+    dbContext.Remove(new Note {Id = id});
+    await dbContext.SaveChangesAsync();
+});
 app.Run();
+
+
+
+class NexusDbContext : DbContext
+{
+    public NexusDbContext(DbContextOptions<NexusDbContext> options)
+    {
+
+    }
+    // TODO: why I need this ?
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=nexus.db");
+    public DbSet<Note> Notes { get; set; }
+}
+
+class Note
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
