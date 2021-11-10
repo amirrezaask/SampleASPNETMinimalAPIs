7bb35bbdf6ca9299c64d6357fb6f9ca335cb8dfe
amirrezaask <raskarpour@gmail.com>
2021-11-10 12:10:44 +0330

    Tests actually :)

---
+++ Nexus.Backend.Tests/BaseHandlerTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nexus.Backend.Tests;
+
+public class BaseHandlerTest
+{
+    internal NexusDbContext _dbContext;
+    public BaseHandlerTest()
+    {
+        var keepAliveConnection = new SqliteConnection("DataSource=:memory:");
+        keepAliveConnection.Open();
+        var options = new DbContextOptionsBuilder<NexusDbContext>().UseSqlite(keepAliveConnection).Options;
+        var dbContext = new NexusDbContext(options);
+        dbContext.Database.EnsureCreated();
+        _dbContext = dbContext;
+    }
+    
+}
---
+++ Nexus.Backend.Tests/NotesTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Nexus.Backend.Handlers;
+using Nexus.Shared.Models;
+using Xunit;
+
+namespace Nexus.Backend.Tests;
+
+public class NotesTests : BaseHandlerTest
+{
+    [Fact]
+    public void GetAllNotesTest()
+    {
+        // Arrange
+        _dbContext.Notes.Add(new Note
+        {
+            Id = Guid.NewGuid().ToString(),
+            Title = "test 1",
+            Content = "test content 1"
+        });
+        _dbContext.SaveChanges();
+        
+        // Act
+        var notes = NotesHandlers.GetNotes(_dbContext).Result;
+        
+        // Assert
+        Assert.Single(notes);
+    }
+
+    [Fact]
+    public void AddNoteTest()
+    {
+        // Act
+        var newID = NotesHandlers.AddNote(_dbContext, new Note
+        {
+            Title = "new title",
+            Content = "new content"
+        }).Result;
+        var note = _dbContext.Notes.Find(newID);
+        // Assert
+        Assert.Equal(newID, note.Id);
+    }
+
+    [Fact]
+    public void GetNote()
+    {
+        var id = Guid.NewGuid().ToString();
+        _dbContext.Add(new Note()
+        {
+            Id = id,
+            Title = "title",
+            Content = "content"
+        });
+        _dbContext.SaveChanges();
+        var note = NotesHandlers.GetNote(id, _dbContext).Result;
+        Assert.Equal(id, note.Id);
+    }
+}
